672348b27988bd8d4f4f5408ba6b0dc0f16d4212
Yuro <martinelliyuri7@gmail.com>
2025-05-21 19:43:49 -0300

    feat: initial movie search screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieSearchPage } from "./features/movies/pages/movie-search.page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieSearchPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { OmdbApiService } from './app/core/services/omdb-api.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    OmdbApiService, // ✅ Add this line
+  ],
+});
