242e39980f6177ce370f0e8f6fe6fe8c5fe8e300
Yuro <martinelliyuri7@gmail.com>
2025-05-21 19:27:13 -0300

    feat: Add new pages for login and movie search

---
+++ src/app/features/movies/pages/movie-search.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-search',
+  imports: [],
+  templateUrl: './movie-search.page.html',
+  styleUrl: './movie-search.page.css'
+})
+export class MovieSearchPage {
+
+}
