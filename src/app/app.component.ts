1668880c6c4187a4740f4a5da8f00409a28f40b7
Yuro <martinelliyuri7@gmail.com>
2025-05-24 11:26:44 -0300

    feat: refactor app component to improve authentication handling and user display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+import { User } from '@angular/fire/auth';
+import { Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'omdb-movie-explorer';
-
+export class AppComponent implements OnInit {
+  user$: Observable<User | null>;
   isAuthReady = false;
 
-  constructor(private readonly auth: AuthService) { }
+  constructor(private readonly auth: AuthService, private readonly router: Router) {
+    this.user$ = this.auth.user$;
+  }
 
   ngOnInit() {
     this.auth.ready$.subscribe(ready => {
       this.isAuthReady = ready;
     });
+  }
+
+  logout() {
+    this.auth.logout().then(() => this.router.navigate(['/login']));
   }
 }
