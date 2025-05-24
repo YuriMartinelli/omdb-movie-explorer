bafcb1a7a042012056cbe8daafe392c8176f8ab2
Yuro <martinelliyuri7@gmail.com>
2025-05-24 11:14:25 -0300

    feat: enhance app component to manage authentication readiness and loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MovieSearchPage } from "./features/movies/pages/movie-search.page";
 import { AuthService } from '@core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MovieSearchPage],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'omdb-movie-explorer';
 
+  isAuthReady = false;
+
   constructor(private readonly auth: AuthService) { }
 
   ngOnInit() {
-    this.auth.ready$.subscribe((ready) => {
-      if (ready) {
-        console.log('✅ Auth ready, user:', this.auth.currentUser);
-      }
+    this.auth.ready$.subscribe(ready => {
+      this.isAuthReady = ready;
     });
   }
 }
