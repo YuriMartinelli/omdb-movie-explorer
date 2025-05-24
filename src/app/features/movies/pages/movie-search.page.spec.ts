aca62c67acb4aa869a6363760beac241f6c904ba
Yuro <martinelliyuri7@gmail.com>
2025-05-23 21:23:36 -0300

    tests:Implements movie search tests

--- src/app/features/movies/pages/movie-search.page.spec.ts
+++ src/app/features/movies/pages/movie-search.page.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { MovieSearchPage } from './movie-search.page';
+import { MockStore } from '@ngrx/store/testing';
+import { provideStore } from '@ngrx/store';
+import { provideRouter, Routes } from '@angular/router';
+import { searchMovies } from '../store/movie.action';
 
 describe('MovieSearchPage', () => {
   let component: MovieSearchPage;
   let fixture: ComponentFixture<MovieSearchPage>;
+  let store: MockStore;
 
+  const routes: Routes = [];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MovieSearchPage]
-    })
-    .compileComponents();
+      imports: [MovieSearchPage, FormsModule],
+      providers: [
+        provideStore(),
+        provideRouter(routes)
+      ]
+    }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(MovieSearchPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the MovieSearchPage component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch searchMovies action on search', () => {
+    spyOn(store, 'dispatch');
+    component.query = 'Inception';
+    component.year = '2010';
+    component.searchMovies();
+    expect(store.dispatch).toHaveBeenCalledWith(searchMovies({ query: 'Inception', year: '2010' }));
+  });
+
+  it('should not dispatch if query is empty', () => {
+    spyOn(store, 'dispatch');
+    component.query = '  ';
+    component.searchMovies();
+    expect(store.dispatch).not.toHaveBeenCalled();
   });
 });
